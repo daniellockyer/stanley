fb64bc0bccd405eb85c850a07d6d24acabdabf6c
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2016-11-17 23:54:09 +0000

    Updated as of Thu 17 Nov 23:44:15 GMT 2016

--- stanley-lib/src/ast.rs
+++ stanley-lib/src/ast.rs
@@ -17,24 +17,22 @@ pub enum BinaryOperator {
 
 impl Debug for Expression {
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
-        use self::Expression::*;
         match *self {
-            BinaryExpression(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
-            Number(n) => write!(fmt, "{:?}", n),
-            BooleanLiteral(b) => write!(fmt, "{:?}", b)
+            Expression::BinaryExpression(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
+            Expression::Number(n) => write!(fmt, "{:?}", n),
+            Expression::BooleanLiteral(b) => write!(fmt, "{:?}", b)
         }
     }
 }
 
 impl Debug for BinaryOperator {
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
-        use self::BinaryOperator::*;
         match *self {
-            Addition => write!(fmt, "+"),
-            Subtraction => write!(fmt, "-"),
-            Multiplication => write!(fmt, "*"),
-            Division => write!(fmt, "/"),
-            Modulo => write!(fmt, "%"),
+            BinaryOperator::Addition => write!(fmt, "+"),
+            BinaryOperator::Subtraction => write!(fmt, "-"),
+            BinaryOperator::Multiplication => write!(fmt, "*"),
+            BinaryOperator::Division => write!(fmt, "/"),
+            BinaryOperator::Modulo => write!(fmt, "%"),
         }
     }
 }
