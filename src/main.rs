136ef58edfec099da55b38a29149e74e0dc1345f
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-03-04 14:43:38 +0000

    Update test file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 #![feature(plugin, custom_attribute)]
 #![plugin(stanlib)]
 #![allow(dead_code)]
-/*
-#[condition(pre="true", post="ret == (x + 1:i32)")]
-fn param_plus_one2(x: i32) -> i32 {
-    x + 1
+
+#[condition(pre="true", post="ret == 15:i32")]
+fn loopy12() -> i32 {
+    let mut a = 0;
+    let mut b = 0;
+
+    while a < 5 {
+        a += 1;
+        b += a;
+    }
+    b
 }
 
-#[condition(pre="true", post="ret == (x - 5:i32)")]
-fn param_minus_five(x: i32) -> i32 {
-    x - 5
+#[condition(pre="x < 100:i32 && x > 10:i32", post="ret == (x < 100:i32 && x > 10:i32)")]
+fn check_range(x:i32) -> bool {
+    x < 100 && x > 10
+}
+
+#[condition(pre="true", post="ret == (x < 100:i32 && x > 10:i32)")]
+fn check_range2(x:i32) -> bool {
+    x < 100 && x > 10
+}
+
+/*********************************************/
+/*
+#[condition(pre="x > 10:i32 && x < 20:i32", post="ret == (x + 1:i32)")]
+fn param_plus_one(x: i32) -> i32 {
+    x + 1
 }
 
 #[condition(pre="true", post="ret == ((x * 2:i32) < 20:i32)")]
@@ -17,16 +36,33 @@ fn double_less_than_twenty(x: i32) -> bool {
     (x * 2) < 20
 }
 
-#[condition(pre="x <= 95:i32", post="ret == (x + 5:i32)")]
-fn add_five(x:i32) -> i32 {
-    assert!(x <= 95);
-    x + 5
+#[condition(pre="true", post="ret == (x / 2:i32)")]
+fn div_two(x:i32) -> i32 {
+    x / 2
 }
 
-#[condition(pre="x > 10:i32 && x < 20:i32", post="ret == (x + 1:i32)")]
-fn param_plus_one(x: i32) -> i32 {
-    assert!(x > 10 && x < 20);
-    x + 1
+#[condition(pre="true", post="ret == !x")]
+fn boolean_not(x:bool) -> bool {
+    if x {
+        false // buggy - change to false
+    } else {
+        true
+    }
+}
+
+#[condition(pre="true", post="ret == (x > (y / 2:i32))")]
+fn bigger_than_input_div_two(x:i32, y:i32) -> bool {
+    x > (y / 2)
+}
+
+#[condition(pre="x > y", post="ret == true")]
+fn bigger_than_input(x:i32, y:i32) -> bool {
+    x > y
+}
+
+#[condition(pre="true", post="ret == (x < 5:i32)")]
+fn check_less_than_five(x:i32) -> bool {
+    x < 5 // buggy - change to 5
 }
 
 #[condition(pre="true", post="ret == (x < 8:i32)")]
@@ -48,20 +84,6 @@ fn negative(x: i32) -> bool {
     x < 0
 }
 
-#[condition(pre="true", post="ret == (x < 5:i32)")]
-fn check_less_than_five(x:i32) -> bool {
-    x < 5 // buggy - change to 5
-}
-
-#[condition(pre="true", post="ret == !x")]
-fn boolean_not(x:bool) -> bool {
-    if x {
-        false // buggy - change to false
-    } else {
-        true
-    }
-}
-
 #[condition(pre="true", post="ret == !x")]
 fn boolean_not3(x:bool) -> bool {
     !x
@@ -76,66 +98,22 @@ fn boolean_not2(x:bool) -> bool {
     }
 }
 
-#[condition(pre="true", post="ret == (x / 2:i32)")]
-fn div_two(x:i32) -> i32 {
-    x / 2
+#[condition(pre="true", post="ret == (x + 1:i32)")]
+fn param_plus_one2(x: i32) -> i32 {
+    x + 1
 }
 
-#[condition(pre="x < 100:i32 && x > 10:i32", post="ret == (x < 100:i32 && x > 10:i32)")]
-fn check_range(x:i32) -> bool {
-    assert!(x < 100 && x > 10);
-    x < 100 && x > 10
+#[condition(pre="true", post="ret == (x + 5:i32)")]
+fn add_five(x:i32) -> i32 {
+    x + 5
 }
 
-#[condition(pre="true", post="ret == (x < 100:i32 && x > 10:i32)")]
-fn check_range2(x:i32) -> bool {
-    x < 100 && x > 10
-}
-
-#[condition(pre="true", post="ret == (x > (y / 2:i32))")]
-fn bigger_than_input_div_two(x:i32, y:i32) -> bool {
-    x > (y / 2)
-}
-
-#[condition(pre="true", post="ret == (x > y)")]
-fn bigger_than_input(x:i32, y:i32) -> bool {
-    x > y
-}
-
-#[condition(pre="true", post="x > (y / 2:i32)")]
-fn weird_bigger_than_input_div_two(x:i32, y:i32) -> bool {
-    x > y
+#[condition(pre="true", post="ret == (x - 5:i32)")]
+fn param_minus_five(x: i32) -> i32 {
+    x - 5
 }
 */
-#[condition(pre="true", post="ret == 10:i32")]
-fn loopy(x: i32) -> i32 {
-    while x < 5 {
-    }
-    10
-}
 
-#[condition(pre="true", post="ret == 10:i32")]
-fn loopy2(x: i32) -> i32 {
-    while x < 5 {
-    }
-    9
-}
-/*
-#[condition(pre="true", post="ret sorted(xs)")]
-fn selection(xs: &mut [u32]) {
-    let (mut i, len) = (0, xs.len());
-    while i < len {
-        let (mut j, mut cur_min) = (i + 1, i);
-        while j < len {
-            if xs[j] < xs[cur_min] {
-                cur_min = j;
-            }
-            j = j + 1;
-        }
-        xs.swap(i, cur_min);
-        i = i + 1;
-    }
-}*/
 
 fn main() {
 }
